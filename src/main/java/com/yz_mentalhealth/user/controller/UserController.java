8d3b17fbca2e2f4b35843dc1acffe15163b775cf
Andy12138 <1208799079@qq.com>
2019-01-21 20:42:42 +0800

    zmg

--- src/main/java/com/yz_mentalhealth/user/controller/UserController.java
+++ src/main/java/com/yz_mentalhealth/user/controller/UserController.java
@@ -7,10 +7,7 @@ import com.yz_mentalhealth.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -64,6 +61,13 @@ public class UserController {
     @PutMapping("/user")
     public String userUpdate(User user){
         userService.save(user);
+        return "redirect:/userList";
+    }
+
+    //删除用户
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") Integer id){
+        userService.delete(id);
         return "redirect:/userList";
     }
 
